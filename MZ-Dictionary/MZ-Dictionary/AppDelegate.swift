1ee99b2c650063c8a54c096b6e9d9d7c1c3fd1d9
jingni1115 <kj227777@naver.com>
2023-11-09 03:33:27 +0900

    [FEAT] : ⚙️ mainviewcontroller 화면 첫 진입으로 뜰 수 있게 설정

--- MZ-Dictionary/MZ-Dictionary/AppDelegate.swift
+++ MZ-Dictionary/MZ-Dictionary/AppDelegate.swift
@@ -10,10 +10,21 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    /**
+     @brief  navigationBarController 객체
+     */
+    var navigationController: UINavigationController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let introVC = MainViewController(nibName: "Main", bundle: nil)
+        navigationController = UINavigationController(rootViewController: introVC)
+        // 네비게이션바 히든
+        navigationController?.isNavigationBarHidden = true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 
