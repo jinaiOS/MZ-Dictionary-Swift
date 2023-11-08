22c8a73a559ad3ac025e04f6de90204fc7a9d430
jingni1115 <kj227777@naver.com>
2023-11-09 03:20:02 +0900

    [FEAT] : ⚙️ main tableview register & tableviewcell 연결

--- MZ-Dictionary/MZ-Dictionary/MainViewController.swift
+++ MZ-Dictionary/MZ-Dictionary/MainViewController.swift
@@ -9,21 +9,46 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    /// main tableview
+    @IBOutlet weak var tvMain: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        registerTableViewCell()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func registerTableViewCell() {
+        tvMain.register(UINib(nibName: "MainTableViweCell", bundle: nil), forCellReuseIdentifier: "MainTableViweCell")
+        tvMain.delegate = self
+        tvMain.dataSource = self
     }
-    */
+}
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViweCell", for: indexPath) as! MainTableViweCell
+        return cell
+    }
+}
 
+class MainTableViweCell: UITableViewCell {
+    
+    /// title label
+    @IBOutlet weak var lblTitle: UILabel!
+    /// content label
+    @IBOutlet weak var lblContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
 }
