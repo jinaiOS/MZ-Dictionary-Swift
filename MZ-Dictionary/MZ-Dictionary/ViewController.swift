300a384edec476a1a0cb014ae16bff0f36ee193b
jingni1115 <kj227777@naver.com>
2023-11-09 02:01:19 +0900

    init xcode

---
+++ MZ-Dictionary/MZ-Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MZ-Dictionary
+//
+//  Created by 김지은 on 2023/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
