b350c8bc910c2839e6306ef320892b9eaac38a93
jingni1115 <kj227777@naver.com>
2023-11-10 20:22:55 +0900

    [FEAT] : ⚙️ 페이징네이션 설정 완료

--- MZ-Dictionary/MZ-Dictionary/MainViewController.swift
+++ MZ-Dictionary/MZ-Dictionary/MainViewController.swift
@@ -6,18 +6,26 @@
 //
 
 import UIKit
-
+//MARK: HAVETO 로딩 맨 아래에선 다시 돌아오도록 설정
 class MainViewController: UIViewController {
-
+    
     /// main tableview
     @IBOutlet weak var tvMain: UITableView!
     
-    var list: [ListModel]?
+    var list: [ListModel] = []
+    
+    var pageNum = 10
+    var totalCount = 0
+    var isTotalData = false
+    var isDataRunning = false
+    
+    var cellHeights: [IndexPath: CGFloat] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         registerTableViewCell()
-        fetchList(pageSize: 1)
+        fetchListCount()
+        fetchList()
     }
     
     func registerTableViewCell() {
@@ -26,23 +34,90 @@ class MainViewController: UIViewController {
         tvMain.dataSource = self
     }
     
-    func fetchList(pageSize: Int) {
-        FireStoreManager.shared.fetchList(pageSize: pageSize) { list in
-            self.list = list
+    func fetchListCount() {
+        Task {
+            let (result, error) = await FireStoreManager.shared.fetchNumberOfDoc()
+            if let error {
+                print(error.localizedDescription)
+                return
+            }
+            totalCount = result ?? 0
+            print("totalCount: \(totalCount)")
+        }
+    }
+    
+    func fetchList() {
+        FireStoreManager.shared.fetchList(pageSize: pageNum) { list in
+            if self.pageNum == 10 {
+                self.list = list ?? []
+            } else {
+                self.list += list ?? []
+            }
+            self.isTotalData = (self.pageNum >= self.totalCount)
             self.tvMain.reloadData()
         }
     }
 }
+extension MainViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.height
+        
+        if offsetY > (contentHeight - height) && isDataRunning == false {
+            isDataRunning = true
+            pageNum += 10
+            if pageNum > totalCount {
+                pageNum = totalCount
+            }
+            
+            if isTotalData == false {
+                DispatchQueue.main.async {
+                    self.tvMain.reloadSections(IndexSet(integer: 1), with: .none)
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.7, execute: {
+                    self.fetchList()
+                })
+            }
+        }
+     }
+}
+
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list?.count ?? 0
+        if section == 0 {
+            return list.count
+        } else if section == 1 && isDataRunning {
+            return 1
+        }
+        
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
-        cell.lblTitle.text = list?[indexPath.row].title ?? ""
-        cell.lblContent.text = list?[indexPath.row].content ?? ""
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
+            cell.lblTitle.text = list[indexPath.row].title ?? ""
+            cell.lblContent.text = list[indexPath.row].content ?? ""
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LoadingCell", for: indexPath) as! LoadingCell
+
+            cell.start()
+            
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        //마지막 인덱스 일때 data 추가 가능 여부를 변경한다.
+        if indexPath.row == list.count - 1 {
+            isDataRunning = false
+        }
     }
 }
 
@@ -57,10 +132,19 @@ class MainTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+    }
+}
+
+class LoadingCell: UITableViewCell {
+    
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    func start() {
+        activityIndicatorView.startAnimating()
     }
 }
